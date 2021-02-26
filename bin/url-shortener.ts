f396c0299bdd33e253d3a4da50fcb3755fb03c23
Chris Chen <chrischen79@gmail.com>
2021-02-26 13:46:22 +1100

    eslint

--- bin/url-shortener.ts
+++ bin/url-shortener.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
+
 import * as cdk from '@aws-cdk/core';
+
 import { UrlShortenerStack } from '../lib/url-shortener-stack';
 
 const app = new cdk.App();
--- test/url-shortener.test.ts
+++ test/url-shortener.test.ts
@@ -1,13 +1,19 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, MatchStyle, matchTemplate } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
+
 import * as UrlShortener from '../lib/url-shortener-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new UrlShortener.UrlShortenerStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new UrlShortener.UrlShortenerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT,
+    ),
+  );
 });
